98f9236f0c935098a8160d66caebff6af7afaac1
Mohammad Khaled Safi <51378060+MohammedSafi96@users.noreply.github.com>
2023-09-13 22:42:51 +0300

    Update ISystemBuilder.cs

--- BuilderDesignPattren/Builder/IBuilder/ISystemBuilder.cs
+++ BuilderDesignPattren/Builder/IBuilder/ISystemBuilder.cs
@@ -1,4 +1,4 @@
-﻿using BuilderDesignPattren.Builder.Product;
+using BuilderDesignPattren.Builder.Product;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +9,12 @@ namespace BuilderDesignPattren.Builder.IBuilder
 {
     internal interface ISystemBuilder
     {
-        void AddMemory(string type);
-        void AddDriver(string type);
-        void AddMouse(string type);
-        void AddKeyboard(string type);
-        void AddTouchScreen(string type);
+        // #Important: Add ISystemBuilder as a return type to achieve Fluent Interface Design Pattern
+        ISystemBuilder AddMemory(string type);
+        ISystemBuilder AddDriver(string type);
+        ISystemBuilder AddMouse(string type);
+        ISystemBuilder AddKeyboard(string type);
+        ISystemBuilder AddTouchScreen(string type);
         ComputerSystem GetSystem();
     }
 }
