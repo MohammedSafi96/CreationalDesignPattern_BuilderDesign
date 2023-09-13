8da35ed84f8d9a663df4552fb0572f512a31fc1c
Mohammad Khaled Safi <51378060+MohammedSafi96@users.noreply.github.com>
2023-09-13 22:44:46 +0300

    Update ConfigurationBuilder.cs

--- BuilderDesignPattren/Builder/Director/ConfigurationBuilder.cs
+++ BuilderDesignPattren/Builder/Director/ConfigurationBuilder.cs
@@ -1,4 +1,4 @@
-﻿using BuilderDesignPattren.Builder.IBuilder;
+using BuilderDesignPattren.Builder.IBuilder;
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -12,11 +12,20 @@ namespace BuilderDesignPattren.Builder.Director
     {
         public void BuildSystem(ISystemBuilder systemBuilder, NameValueCollection collection)
         {
-            systemBuilder.AddDriver(collection["HDDSize"]);
-            systemBuilder.AddMemory(collection["RAM"]);
-            systemBuilder.AddKeyboard(collection["Keyboard"]);
-            systemBuilder.AddMouse(collection["Mouse"]);
-            systemBuilder.AddTouchScreen(collection["TouchScreen"]);
+            // #Important: By ISystemBuilder as a return type we can get the methods by calling the functions values recursively like the LINQ, that is the Fluent Interface Design Pattern
+            // Fluent Interface Design Pattern
+            systemBuilder.AddDriver(collection["HDDSize"])
+            .AddMemory(collection["RAM"])
+            .AddKeyboard(collection["Keyboard"])
+            .AddMouse(collection["Mouse"])
+            .AddTouchScreen(collection["TouchScreen"]);
+
+            // normal method
+            //systemBuilder.AddDriver(collection["HDDSize"]);
+            //systemBuilder.AddMemory(collection["RAM"]);
+            //systemBuilder.AddKeyboard(collection["Keyboard"]);
+            //systemBuilder.AddMouse(collection["Mouse"]);
+            //systemBuilder.AddTouchScreen(collection["TouchScreen"]);
         }
     }
 }
